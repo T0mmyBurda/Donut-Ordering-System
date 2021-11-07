81ec71b9793715b3396cbbd29c8554c108d028eb
T0mmyBurda <31544632+T0mmyBurda@users.noreply.github.com>
2021-11-06 21:45:40 -0400

    Expanded sales page

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 let donutsInCart = 0;
+let order = []
 
 function donutSelected(type) {
     donutsInCart++;
-    // console.log('d' + donutsInCart)
+    order.push(type);
+    console.log(order);
     document.getElementById('d' + donutsInCart).src='img/carrossel/carrossel-' + type + '.svg';
+}
+
+function donutDeselected(btn) {
+    donutsInCart--;
+    console.log('H' , order);
+    order.pop(btn); //somthing wrong here
+    console.log('H' , order);
+    document.getElementById('d' + btn).src='img/empty.svg';
+}
+
+function donutHover(btn) {
+    console.log(order)
+    if(btn <= donutsInCart){
+        document.getElementById('d' + btn).src='img/xDonuts/xDonut-' + order[btn - 1] + '.svg';
+    }
+}
+
+function donutUnhover(btn) {
+    console.log(donutsInCart);
+    console.log(btn);
+    if(btn <= donutsInCart){
+        document.getElementById('d' + btn).src='img/carrossel/carrossel-' + order[btn - 1] + '.svg';
+    }
 }

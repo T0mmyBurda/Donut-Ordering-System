5e6f162da5de44e57a1c736aa82b6ebb5999b00b
T0mmyBurda <31544632+T0mmyBurda@users.noreply.github.com>
2021-11-07 22:31:03 -0500

    streamlined sales page JS

--- main.js
+++ main.js
@@ -5,28 +5,64 @@ function donutSelected(type) {
     donutsInCart++;
     order.push(type);
     console.log(order);
-    document.getElementById('d' + donutsInCart).src='img/carrossel/carrossel-' + type + '.svg';
+    // document.getElementById('d' + donutsInCart).src='img/carrossel/carrossel-' + type + '.svg';
+    displayDonuts();
 }
 
 function donutDeselected(btn) {
     donutsInCart--;
     console.log('H' , order);
-    order.pop(btn); //somthing wrong here
+    if((btn - 1) == donutsInCart) {
+        order.pop();
+    }
+    else {
+        order.splice(btn - 1 , 1); //need
+    }
     console.log('H' , order);
-    document.getElementById('d' + btn).src='img/empty.svg';
+    displayDonuts();
+    // document.getElementById('d' + btn).src='img/empty.svg';
+
+    // for(i = btn ; i < donutsInCart + 2 ; i++) {
+    //     if(order[i + 1] != undefined) {
+            
+    //     }
+    //     document.getElementById('d' + i).src='img/empty.svg';
+    // }
 }
 
 function donutHover(btn) {
-    console.log(order)
-    if(btn <= donutsInCart){
-        document.getElementById('d' + btn).src='img/xDonuts/xDonut-' + order[btn - 1] + '.svg';
-    }
+    // console.log(order)
+    // if(btn <= donutsInCart){
+    //     document.getElementById('d' + btn).src='img/xDonuts/xDonut-' + order[btn - 1] + '.svg';
+    // }
+    order[btn - 1] = order[btn - 1] * -1;
+    displayDonuts();
+    console.log(order);
 }
 
 function donutUnhover(btn) {
-    console.log(donutsInCart);
-    console.log(btn);
-    if(btn <= donutsInCart){
-        document.getElementById('d' + btn).src='img/carrossel/carrossel-' + order[btn - 1] + '.svg';
+    // console.log(donutsInCart);
+    // console.log(btn);
+    // if(btn <= donutsInCart){
+    //     document.getElementById('d' + btn).src='img/carrossel/carrossel-' + order[btn - 1] + '.svg';
+    // }
+    //when first unhover cannot be negative
+    order[btn - 1] = Math.abs(order[btn - 1]);
+    displayDonuts();
+    console.log(order);
+}
+
+function displayDonuts() {
+    //order = [1 , 2 , 3 ,4 , 5 , 6 , 1 , 2 , 3 ,4 , 5 , 6]
+    for(i = 0 ; i < 12 ; i++) {
+        if(i >= order.length) {
+            document.getElementById('d' + (i + 1)).src='img/empty.svg';
+        }
+        else if(order[i] < 0) {
+            document.getElementById('d' + (i + 1)).src='img/xDonuts/xDonut-' + (order[i] * -1) + '.svg';
+        }
+        else {
+            document.getElementById('d' + (i + 1)).src='img/carrossel/carrossel-' + order[i] + '.svg';
+        }
     }
 }
